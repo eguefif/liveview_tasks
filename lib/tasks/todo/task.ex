22fa341deb22257a17db29452170d96ae41c3ede
Emmanuel Guefif <eguefif@fastmail.com>
2025-03-20 17:46:16 -0400

    TASKS-1: add schema for task

---
+++ lib/tasks/todo/task.ex
@@ -0,0 +1,18 @@
+defmodule Hello.Todo.Task do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "todo_task" do
+    field :done, :boolean, default: false
+    field :task, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(task, attrs) do
+    task
+    |> cast(attrs, [:task, :done])
+    |> validate_required([:task, :done])
+  end
+end
