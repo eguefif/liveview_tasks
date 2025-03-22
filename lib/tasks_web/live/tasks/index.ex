a5bbf28d996c0f3d2388af92b2a6bad2eeb1e3ca
Emmanuel Guefif <eguefif@fastmail.com>
2025-03-21 21:12:03 -0400

    TASKS-3: add form to create task

--- lib/tasks_web/live/tasks/index.ex
+++ lib/tasks_web/live/tasks/index.ex
@@ -1,21 +1,75 @@
 defmodule TasksWeb.TasksLive.Index do
   use TasksWeb, :live_view
   alias Tasks.Todo
+  alias Tasks.Todo.Task
 
+  @impl true
   def render(assigns) do
     ~H"""
-    <.table id="tasks" rows={@tasks}>
-      <:col :let={task} label="Task">{task.task}</:col>
-      <:col :let={task} label="Done">{task.done}</:col>
-      <:action :let={task}>
-        X
-      </:action>
-    </.table>
+    <div>
+      <.simple_form for={@form} id="task-form" phx-change="validate" phx-submit="save">
+        <.input field={@form[:task]} type="text" label="Task" />
+      </.simple_form>
+
+      <.table id="tasks" rows={@tasks}>
+        <:col :let={task} label="Task">{task.task}</:col>
+        <:col :let={task} label="Done">{task.done}</:col>
+        <:action :let={task}>
+          <.link
+            phx-click={JS.push("delete", value: %{id: task.id}) |> hide("##{task.id}")}
+            data-confirm="Are you sure?"
+          >
+            X
+          </.link>
+        </:action>
+      </.table>
+    </div>
     """
   end
 
+  @impl true
   def mount(_params, _session, socket) do
     tasks = Todo.list_tasks()
-    {:ok, assign(socket, :tasks, tasks)}
+
+    socket =
+      socket
+      |> assign(:tasks, tasks)
+      |> assign(:form, to_form(Todo.change_task(%Task{})))
+
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_event("validate", %{"task" => task}, socket) do
+    form =
+      %Task{}
+      |> Todo.change_task(task)
+      |> to_form(action: :validate)
+
+    socket =
+      socket
+      |> assign(:tasks, Todo.list_tasks())
+      |> assign(:form, form)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("save", %{"task" => task}, socket) do
+    Todo.create_task(task)
+
+    socket =
+      socket
+      |> assign(:tasks, Todo.list_tasks())
+      |> assign(:form, to_form(Todo.change_task(%Task{})))
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    Todo.delete_task(id)
+    tasks = Todo.list_tasks()
+    {:noreply, assign(socket, :tasks, tasks)}
   end
 end
